540238bc380dbbafd1e5fe04fe34d830119bda97
William05Cardozo <3784@holbertonschool.com>
2021-12-08 08:04:39 -0800

    BettyCorrection

--- built_in.c
+++ built_in.c
@@ -12,7 +12,6 @@ char (*built(list_t *m))(list_t *m)
 
 	built_t op[] = {
 		{"exit", escape},
-		/*{"env", _env},*/
 		{NULL, NULL}
 	};
 
@@ -36,25 +35,5 @@ char escape(__attribute__((unused)) list_t *list)
 {
 	free(list->buffer);
 	free(list->array);
-	exit(98);
-}
-
-
-/**
- * _env - function
- * @list: Linked list
- * i: iterator
- * Return: No line
- 
-
-char _env(__attribute__((unused)) list_t *list)
-{
-	unsigned int i;
-
-	for (i = 0; list->env[i]; i++)
-	{
-		_puts(list->env[i]);
-		_puts("\n");
-	}
 	return (0);
-} */
+}
